60795ac5788b77dc605c1515e77c88459f3f6b81
Kun Lee <kunlee1218@qq.com>
2019-09-24 10:45:00 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -6,8 +6,8 @@ def cosine_similarity(feature):
         dist: per-point square distance, [B, N, N]
     """
     B, N, C = feature.shape
-    feat_ = torch.matmul(feature, feature.permute(0, 2, 1))  # [B, N, N]
-    norm = torch.sum(feature ** 2, -1).view(B, N, 1)
+    feat = torch.matmul(feature, feature.permute(0, 2, 1))  # [B, N, N]
+    norm = torch.sqrt(torch.sum(feature ** 2, -1)).view(B, N, 1)
     norm = torch.matmul(norm, norm.permute(0, 2, 1))
-    res = torch.div(feat_, norm)
+    res = torch.div(feat, norm)
     return res
